60d63f4e88d71e2b7f2921a7898ffbf8d5510483
Luis <luisc.suareza@konradlorenz.edu.co>
2020-08-23 18:21:17 -0500

    Project Creation
    Initial commit with the flutter template for the prototype.

---
+++ android/app/src/main/kotlin/com/express/mr_food_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.express.mr_food_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
