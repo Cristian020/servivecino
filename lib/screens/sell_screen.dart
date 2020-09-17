7600c6f36be467b4c9fa8f7ad560e9be7bb2dda9
Laura <mbernallaura>
2020-09-17 17:38:48 -0500

    Text

--- lib/screens/sell_screen.dart
+++ lib/screens/sell_screen.dart
@@ -9,11 +9,6 @@ class FavoriteScreen extends StatefulWidget {
   _FavoriteScreenState createState() => _FavoriteScreenState();
 }
 
-
-
-
-
-
 class _FavoriteScreenState extends State<FavoriteScreen>
     with AutomaticKeepAliveClientMixin<FavoriteScreen> {
 
