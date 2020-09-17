37679caa061f6f12990b9fde8e92ffd6a32e5142
Cristian <cristianc.martinezv020@gmail.com>
2020-09-17 16:29:26 -0500

    the chat contacts

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -9,20 +9,55 @@ class _ChatsState extends State<Chats> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: Icon(
-            Icons.keyboard_backspace,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: Icon(
+              Icons.keyboard_backspace,
+            ),
+            onPressed: () => Navigator.pop(context),
           ),
-          onPressed: () => Navigator.pop(context),
+          centerTitle: true,
+          title: Text("Conversaciones"),
+          elevation: 0.0,
         ),
-        centerTitle: true,
-        title: Text(
-          "Conversaciones",
-        ),
-        elevation: 0.0,
-      ),
-    );
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              //    onTap: () {Navigator.pushNamed(context, 'chat_carl');},
+              leading: Icon(Icons.account_circle),
+              title: Text('Carlos'),
+              subtitle: Text('Buenas tardes. Me gustaria saber ...'),
+              trailing: Icon(Icons.done),
+            ),
+            ListTile(
+              /* onTap: () {
+                Navigator.pushNamed(context, 'tab4');
+              },*/
+              leading: Icon(Icons.account_circle),
+              title: Text('Laura'),
+              subtitle: Text('Buenas dias. Cuanto cuesta la hora...'),
+              trailing: Icon(Icons.done_all),
+            ),
+            ListTile(
+              /* onTap: () {
+                Navigator.pushNamed(context, 'tab5');
+              },*/
+              leading: Icon(Icons.account_circle),
+              title: Text('Jorge'),
+              subtitle: Text('Hola! Cuanto tarda el arreglo? '),
+              trailing: Icon(Icons.replay),
+            ),
+            ListTile(
+              /* onTap: () {
+                Navigator.pushNamed(context, 'tab4');
+              },*/
+              leading: Icon(Icons.account_circle),
+              title: Text('Fabian'),
+              subtitle: Text('No. Esa maquina no tiene...'),
+              trailing: Icon(Icons.done_all),
+            )
+          ],
+        ));
   }
 }
