16aa101fca872a563ceb5c24bd8f36e170830fea
Luis <luisc.suareza@konradlorenz.edu.co>
2020-09-29 18:46:49 -0500

    Home can see all services added in firebase
    Home can see all services added in firebase

--- lib/util/services.dart
+++ lib/util/services.dart
@@ -1,3 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> readLoacationData() async {
+  var query = await Firestore.instance
+      .collection('formulario')
+      .getDocuments()
+      .then((querySnapshot) {
+    List<Map> items = [];
+    querySnapshot.documents.forEach((result) {
+      var info = result.data();
+      items.add({"img": info["Imagen"], "name": info["Titulo"]});
+      //print(result.data());
+    });
+    foods = items;
+  });
+}
+
 List<Map> foods = [
-  {"img": "assets/servicio_demo.jpg", "name": "Servicio Demo"},
+  {"img": "https://picsum.photos/250?image=9", "name": "Servicio Demo"},
 ];
