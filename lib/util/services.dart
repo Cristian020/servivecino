4a7c8c95bf0e2287ff4b99f2961563006f07dc79
Luis <luisc.suareza@konradlorenz.edu.co>
2020-11-12 17:46:12 -0500

    Back to latitude longitude system
    Back to latitude longitude system

--- lib/util/services.dart
+++ lib/util/services.dart
@@ -1,19 +1,46 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:restaurant_ui_kit/util/const.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 
 double latitude;
 double longitude;
+double radius = 50;
+String field = 'Geopoint';
+final geo = Geoflutterfire();
+final _firestore = FirebaseFirestore.instance;
 
 Future<void> readServicesData() async {
   Constants().validateLatitude().then((value) => {latitude = value});
   Constants().validateLongitude().then((value) => {longitude = value});
+
+// Create a geoFirePoint
+  // GeoFirePoint center = geo.point(latitude: latitude, longitude: longitude);
+  // var collectionReference = _firestore.collection('formulario');
+  // Stream<List<DocumentSnapshot>> stream = geo
+  //     .collection(collectionRef: collectionReference)
+  //     .within(center: center, radius: radius, field: field, strictMode: true);
+  // stream.listen((List<DocumentSnapshot> documentList) {
+  //   print(stream.length);
+  //   print(documentList[0].data());
+  //   List<Map> items = [];
+  //   documentList.forEach((DocumentSnapshot document) {
+  //     var info = document.data();
+  //     items.add({
+  //       "img": info["Imagen"],
+  //       "name": info["Titulo"],
+  //       "desc": info['Descripcion'],
+  //       "precio": info['Precio'],
+  //       "celular": info['Celular'],
+  //       "categoria": info['Categoria'],
+  //       "dir": info['Direccion'],
+  //     });
+  //     print(document.data());
+  //   });
+  //   services = items;
+  // });
+
   var query = await Firestore.instance
       .collection('formulario')
-      .where('latitud',
-          isGreaterThan: latitude - 0.02, isLessThanOrEqualTo: latitude + 0.02)
-      // .where('longitud',
-      //     isGreaterThanOrEqualTo: longitude - 0.02,
-      //     isLessThanOrEqualTo: longitude + 0.02)
       .getDocuments()
       .then((querySnapshot) {
     List<Map> items = [];
@@ -35,5 +62,5 @@ Future<void> readServicesData() async {
 }
 
 List<Map> services = [
-  {"img": "https://picsum.photos/250?image=9", "name": "Servicio Demo"},
+  //{"img": "https://picsum.photos/250?image=9", "name": "Servicio Demo"},
 ];
