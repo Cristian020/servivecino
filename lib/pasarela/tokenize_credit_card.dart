09004e6e62d928689e3082a3c4a72e4e1d5a3a9d
Luis <luisc.suareza@konradlorenz.edu.co>
2020-11-20 11:38:39 -0500

    Updated the credit card tokenizing
    Updated the credit card tokenizing

--- lib/pasarela/tokenize_credit_card.dart
+++ lib/pasarela/tokenize_credit_card.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:restaurant_ui_kit/pasarela/asociate_credit_cart_to_customer.dart';
+import 'package:restaurant_ui_kit/util/const.dart';
 
-Future<http.Response> createTokenize() async {
+String _tokenCc;
+Future<http.Response> createTokenize(
+    String name, String month, String year, String number) async {
   final http.Response response = await http.post(
     'https://sandbox.tpaga.co/api/tokenize/credit_card',
     headers: <String, String>{
@@ -12,16 +15,19 @@ Future<http.Response> createTokenize() async {
     },
     body: jsonEncode(<String, String>{
       //aca toca reemplazar los datos que estan quemados
-      "cardHolderName": "Luis S.",
-      "expirationMonth": "12",
-      "expirationYear": "2030",
-      "primaryAccountNumber": "4111111111111111"
+      "cardHolderName": name,
+      "expirationMonth": month,
+      "expirationYear": "20" + year,
+      "primaryAccountNumber": number.trim()
     }),
   );
 
   if (response.statusCode == 201) {
     //Aca que hacemos en caso de tener 201 la creacion de la tarjeta es exitosa y continuamos
-    createAsociate();
+    final responseJson = json.decode(response.body)['token'];
+    _tokenCc = responseJson;
+    print("Este el el json de response: " + responseJson.toString());
+    Constants().setTokenCc(_tokenCc);
   } else {
     throw Exception('Failed to create');
   }
