7d3080878f6af1a026e34a72da270fc69ee20688
Sebastian <novas.lobog@konradlorenz.edu.co>
2020-11-19 20:45:55 -0500

    delet garbash funtion

--- lib/util/shoppingCart_services.dart
+++ lib/util/shoppingCart_services.dart
@@ -1,33 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:restaurant_ui_kit/util/const.dart';
-
-String keyword = "";
-
-List<Map> shoppingCart = [
-  {"img": "https:// picsum.photos/250?image=9", "name": "Servicio Demo"},
-];
-
-Future<void> readshoppingCartServicesData() async {
-  var query = await Firestore.instance
-      .collection('cart')
-      .getDocuments()
-      .then((querySnapshot) {
-    List<Map> items = [];
-    querySnapshot.documents.forEach((result) {
-      var info = result.data();
-      items.add({
-        "img": info["Imagen"],
-        "name": info["Titulo"],
-        "desc": info['Descripcion'],
-        "precio": info['Precio'],
-        "celular": info['Celular'],
-        "categoria": info['Categoria'],
-        "dir": info['Direccion'],
-      });
-      //print(result.data());
-    });
-    shoppingCart = items;
-  });
-}
+List<Map> shoppingCart = [];
 
 Future<void> addshoppingCartServicesData() async {}
