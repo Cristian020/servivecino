59d3d5b5f171250810afe710cc76bd244af07a04
Cristian <cristianc.martinezv020@gmail.com>
2020-12-03 11:10:00 -0500

    animations
    animations in profile, sell_screen view

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -4,6 +4,7 @@ import 'package:restaurant_ui_kit/providers/app_provider.dart';
 import 'package:restaurant_ui_kit/screens/splash.dart';
 import 'package:restaurant_ui_kit/util/const.dart';
 import 'package:restaurant_ui_kit/util/info_services.dart';
+import 'package:animate_do/animate_do.dart';
 
 class Profile extends StatefulWidget {
   final int index;
@@ -18,170 +19,172 @@ class _ProfileState extends State<Profile> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-        child: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                  child: Image.asset(
-                    "assets/vecino.png",
-                    fit: BoxFit.cover,
-                    width: 100.0,
-                    height: 100.0,
+        child: ElasticInDown(
+          child: ListView(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Image.asset(
+                      "assets/vecino.png",
+                      fit: BoxFit.cover,
+                      width: 100.0,
+                      height: 100.0,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "Usuario:",
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "${infoServices[0]['name']}" +
-                                " " +
-                                "${infoServices[0]['lastName']}",
-                            //user,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 20.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          InkWell(
-                            onTap: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (BuildContext context) {
-                                    return SplashScreen();
-                                  },
-                                ),
-                              );
-                            },
-                            child: Text(
-                              "Cerrar sesion",
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Usuario:",
                               style: TextStyle(
-                                fontSize: 18.5,
-                                fontWeight: FontWeight.w900,
-                                color: Theme.of(context).accentColor,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                        SizedBox(height: 5.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "${infoServices[0]['name']}" +
+                                  " " +
+                                  "${infoServices[0]['lastName']}",
+                              //user,
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) {
+                                      return SplashScreen();
+                                    },
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "Cerrar sesion",
+                                style: TextStyle(
+                                  fontSize: 18.5,
+                                  fontWeight: FontWeight.w900,
+                                  color: Theme.of(context).accentColor,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    flex: 3,
                   ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Divider(),
-            Container(height: 15.0),
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Text(
-                "Informacion de cuenta".toUpperCase(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
+                ],
               ),
-            ),
-            ListTile(
-              title: Text(
-                "Documento",
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w700,
+              Divider(),
+              Container(height: 15.0),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  "Informacion de cuenta".toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              subtitle: Text(
-                "${infoServices[0]['document']}",
-              ),
-            ),
-            ListTile(
-              title: Text(
-                "Direccion",
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w700,
+              ListTile(
+                title: Text(
+                  "Documento",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                subtitle: Text(
+                  "${infoServices[0]['document']}",
                 ),
               ),
-              subtitle: Text(
-                "${infoServices[0]['direccion']}",
-              ),
-            ),
-            ListTile(
-              title: Text(
-                "Telefono",
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w700,
+              ListTile(
+                title: Text(
+                  "Direccion",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                subtitle: Text(
+                  "${infoServices[0]['direccion']}",
                 ),
               ),
-              subtitle: Text(
-                "${infoServices[0]['telefono']}",
-              ),
-            ),
-            ListTile(
-              title: Text(
-                "Correo",
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w700,
+              ListTile(
+                title: Text(
+                  "Telefono",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                subtitle: Text(
+                  "${infoServices[0]['telefono']}",
                 ),
               ),
-              subtitle: Text(
-                "${infoServices[0]['email']}",
-              ),
-            ),
-            ListTile(
-              title: Text(
-                "Tema oscuro",
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w700,
+              ListTile(
+                title: Text(
+                  "Correo",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                subtitle: Text(
+                  "${infoServices[0]['email']}",
                 ),
               ),
-              trailing: Switch(
-                value: Provider.of<AppProvider>(context).theme ==
-                        Constants.lightTheme
-                    ? false
-                    : true,
-                onChanged: (v) async {
-                  if (v) {
-                    Provider.of<AppProvider>(context, listen: false)
-                        .setTheme(Constants.darkTheme, "dark");
-                  } else {
-                    Provider.of<AppProvider>(context, listen: false)
-                        .setTheme(Constants.lightTheme, "light");
-                  }
-                },
-                activeColor: Theme.of(context).accentColor,
+              ListTile(
+                title: Text(
+                  "Tema oscuro",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                trailing: Switch(
+                  value: Provider.of<AppProvider>(context).theme ==
+                          Constants.lightTheme
+                      ? false
+                      : true,
+                  onChanged: (v) async {
+                    if (v) {
+                      Provider.of<AppProvider>(context, listen: false)
+                          .setTheme(Constants.darkTheme, "dark");
+                    } else {
+                      Provider.of<AppProvider>(context, listen: false)
+                          .setTheme(Constants.lightTheme, "light");
+                    }
+                  },
+                  activeColor: Theme.of(context).accentColor,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
